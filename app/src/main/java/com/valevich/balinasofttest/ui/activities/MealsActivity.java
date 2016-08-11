5a5ab3ed5a373e8460efb9f41843f0fb9b4f9cb8
Serge Valevich <romsolog@mail.ru>
2016-08-11 14:49:55 +0300

    manifest

---
+++ app/src/main/java/com/valevich/balinasofttest/ui/activities/MealsActivity.java
@@ -0,0 +1,12 @@
+package com.valevich.balinasofttest.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.valevich.balinasofttest.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_meals)
+public class MealsActivity extends AppCompatActivity {
+
+}

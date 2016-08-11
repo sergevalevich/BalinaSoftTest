5416608896b42181c4be3837e65483c9980d8f67
Serge Valevich <romsolog@mail.ru>
2016-08-11 18:28:17 +0300

    stub fragments

---
+++ app/src/main/java/com/valevich/balinasofttest/ui/fragments/MealsFragment.java
@@ -0,0 +1,11 @@
+package com.valevich.balinasofttest.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+import com.valevich.balinasofttest.R;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.fragment_meals)
+public class MealsFragment extends Fragment{
+}
